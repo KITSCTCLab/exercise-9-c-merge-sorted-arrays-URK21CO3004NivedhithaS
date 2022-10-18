d7130fb7a7282c2a2b659d8882dc4dc834fd302b
URK21CO3004NivedhithaS <110015365+URK21CO3004NivedhithaS@users.noreply.github.com>
2022-10-18 12:31:11 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,16 @@ from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   # Write code here
+  nums1=[]
+  nums2=[]
+  for item in input().spilt(","):
+  num1.append(int(item))
+  for item in input().spilt(','):
+    nums2.appemd(int(item))
+    m=int(input())
+    n=int(input))
+    merge(nums1,m,nums2,n)
+    print(nums1)
 
 
 # Do not change the following code
